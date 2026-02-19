d4f3178557b26ec944be9014f5ae8026df2b671a
tjk144 <tkovach@wisc.edu>
2026-02-19 10:55:47 -0600

    Made the parser human interprettable

--- autotuner/compiler/include/falcon-atc/AST.hpp
+++ autotuner/compiler/include/falcon-atc/AST.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <map>
 #include <memory>
 #include <string>
 #include <variant>
@@ -26,6 +25,11 @@ struct ParamDecl {
   std::string name;
   ParamType type;
   std::shared_ptr<Value> default_value;
+  static ParamDecl *make(int type, std::string *name) {
+    auto *decl = new ParamDecl{std::move(*name), (ParamType)type, nullptr};
+    delete name;
+    return decl;
+  }
 };
 
 struct SpecDecl {
@@ -33,6 +37,21 @@ struct SpecDecl {
   std::string name;
   std::string
       parameter; // e.g. "plunger_gate" in "Quantity pinchoff [plunger_gate]"
+  SpecDecl(ParamType t, std::string n, std::string p = "")
+      : type(t), name(std::move(n)), parameter(std::move(p)) {}
+
+  static SpecDecl *make(int type, std::string *name,
+                        std::string *param = nullptr) {
+    SpecDecl *s;
+    if (param)
+      s = new SpecDecl((ParamType)type, std::move(*name), std::move(*param));
+    else
+      s = new SpecDecl((ParamType)type, std::move(*name), "");
+    delete name;
+    if (param)
+      delete param;
+    return s;
+  }
 };
 
 class Expr {
@@ -49,6 +68,23 @@ public:
   std::unique_ptr<Expr> clone() const override {
     return std::make_unique<ConstExpr>(value);
   }
+
+  static ConstExpr *from_int(std::string *v) {
+    auto *c = new ConstExpr(std::stoll(*v));
+    delete v;
+    return c;
+  }
+  static ConstExpr *from_double(std::string *v) {
+    auto *c = new ConstExpr(std::stod(*v));
+    delete v;
+    return c;
+  }
+  static ConstExpr *from_string(std::string *v) {
+    auto *c = new ConstExpr(std::move(*v));
+    delete v;
+    return c;
+  }
+  static ConstExpr *from_bool(bool b) { return new ConstExpr(b); }
 };
 
 class VarExpr : public Expr {
@@ -57,6 +93,18 @@ public:
   VarExpr(std::string n) : name(std::move(n)) {}
   std::unique_ptr<Expr> clone() const override {
     return std::make_unique<VarExpr>(name);
+  }
+
+  static VarExpr *make(std::string *n) {
+    auto *v = new VarExpr(std::move(*n));
+    delete n;
+    return v;
+  }
+  static VarExpr *make_config() { return new VarExpr("config"); }
+  static VarExpr *make_next(std::string *n) {
+    auto *v = new VarExpr("next " + *n);
+    delete n;
+    return v;
   }
 };
 
@@ -81,6 +129,11 @@ public:
   std::unique_ptr<Expr> clone() const override {
     return std::make_unique<BinaryExpr>(op, left->clone(), right->clone());
   }
+
+  static BinaryExpr *make(std::string op, Expr *left, Expr *right) {
+    return new BinaryExpr(std::move(op), std::unique_ptr<Expr>(left),
+                          std::unique_ptr<Expr>(right));
+  }
 };
 
 class UnaryExpr : public Expr {
@@ -89,8 +142,12 @@ public:
   std::unique_ptr<Expr> expr;
   UnaryExpr(std::string o, std::unique_ptr<Expr> e)
       : op(std::move(o)), expr(std::move(e)) {}
-  std::unique_ptr<Expr> clone() const override {
+  [[nodiscard]] std::unique_ptr<Expr> clone() const override {
     return std::make_unique<UnaryExpr>(op, expr->clone());
+  }
+
+  static UnaryExpr *make(std::string op, Expr *expr) {
+    return new UnaryExpr(std::move(op), std::unique_ptr<Expr>(expr));
   }
 };
 
@@ -100,11 +157,27 @@ public:
   std::vector<std::unique_ptr<Expr>> args;
   CallExpr(std::string n, std::vector<std::unique_ptr<Expr>> a)
       : name(std::move(n)), args(std::move(a)) {}
-  std::unique_ptr<Expr> clone() const override {
+  [[nodiscard]] std::unique_ptr<Expr> clone() const override {
     std::vector<std::unique_ptr<Expr>> cloned_args;
     for (const auto &arg : args)
       cloned_args.push_back(arg->clone());
     return std::make_unique<CallExpr>(name, std::move(cloned_args));
+  }
+  static std::vector<std::unique_ptr<Expr>>
+  to_unique_vec(std::vector<Expr *> *v) {
+    std::vector<std::unique_ptr<Expr>> res;
+    if (v) {
+      for (auto e : *v)
+        res.push_back(std::unique_ptr<Expr>(e));
+    }
+    return res;
+  }
+
+  static CallExpr *make(std::string *name, std::vector<Expr *> *args) {
+    auto *c = new CallExpr(std::move(*name), to_unique_vec(args));
+    delete name;
+    delete args;
+    return c;
   }
 };
 
@@ -115,6 +188,12 @@ struct Assignment {
   Assignment(std::vector<std::string> t, std::unique_ptr<Expr> expr)
       : targets(std::move(t)), expression(std::move(expr)) {}
 
+  static Assignment *make(std::vector<std::string> *idents,
+                          falcon::atc::Expr *expr) {
+    auto *a = new Assignment{std::move(*idents), std::unique_ptr<Expr>(expr)};
+    delete idents;
+    return a;
+  }
   Assignment(Assignment &&) noexcept = default;
   Assignment &operator=(Assignment &&) noexcept = default;
   Assignment(const Assignment &) = delete;
@@ -124,12 +203,27 @@ struct Assignment {
 struct Mapping {
   std::string src;
   std::string dst;
+
+  Mapping(std::string s, std::string d)
+      : src(std::move(s)), dst(std::move(d)) {}
+
+  static Mapping *make(std::string *src, std::string *dst = nullptr) {
+    Mapping *m;
+    if (dst)
+      m = new Mapping(std::move(*src), std::move(*dst));
+    else
+      m = new Mapping(std::move(*src), *src); // dst = src if not provided
+    delete src;
+    if (dst)
+      delete dst;
+    return m;
+  }
 };
 
 struct TransitionTarget {
   std::string autotuner_name;
   std::string state_name;
-  std::unique_ptr<Expr> parameter; // e.g. state[next plunger_gate]
+  std::unique_ptr<Expr> parameter;
   std::vector<Mapping> mappings;
 
   TransitionTarget(std::string at, std::string st, std::unique_ptr<Expr> p,
@@ -137,9 +231,46 @@ struct TransitionTarget {
       : autotuner_name(std::move(at)), state_name(std::move(st)),
         parameter(std::move(p)), mappings(std::move(m)) {}
 
-  TransitionTarget() = default;
-  TransitionTarget(TransitionTarget &&) noexcept = default;
-  TransitionTarget &operator=(TransitionTarget &&) noexcept = default;
+  // Static factory methods for Bison
+  static TransitionTarget *
+  from_state_expr_mappings(std::string *state, Expr *expr,
+                           std::vector<Mapping> *mappings) {
+    auto *t =
+        new TransitionTarget{"", std::move(*state), std::unique_ptr<Expr>(expr),
+                             std::move(*mappings)};
+    delete state;
+    delete mappings;
+    return t;
+  }
+  static TransitionTarget *from_state_mappings(std::string *state,
+                                               std::vector<Mapping> *mappings) {
+    auto *t = new TransitionTarget{"", std::move(*state), nullptr,
+                                   std::move(*mappings)};
+    delete state;
+    delete mappings;
+    return t;
+  }
+  static TransitionTarget *
+  from_ns_state_expr_mappings(std::string *ns, std::string *state, Expr *expr,
+                              std::vector<Mapping> *mappings) {
+    auto *t =
+        new TransitionTarget{std::move(*ns), std::move(*state),
+                             std::unique_ptr<Expr>(expr), std::move(*mappings)};
+    delete ns;
+    delete state;
+    delete mappings;
+    return t;
+  }
+  static TransitionTarget *
+  from_ns_state_mappings(std::string *ns, std::string *state,
+                         std::vector<Mapping> *mappings) {
+    auto *t = new TransitionTarget{std::move(*ns), std::move(*state), nullptr,
+                                   std::move(*mappings)};
+    delete ns;
+    delete state;
+    delete mappings;
+    return t;
+  }
 };
 
 struct Transition {
@@ -185,12 +316,43 @@ struct StateDecl {
   StateDecl &operator=(StateDecl &&) noexcept = default;
   StateDecl(const StateDecl &) = delete;
   StateDecl &operator=(const StateDecl &) = delete;
+  static StateDecl *make(std::string *name, std::vector<ParamDecl> *params,
+                         std::vector<ParamDecl> *temps, Expr *measurement,
+                         std::vector<Transition> *transitions,
+                         std::string *param = nullptr) {
+    auto param_val = param ? std::move(*param) : "";
+    auto *s = new StateDecl(std::move(*name), std::move(param_val),
+                            std::move(*params), std::move(*temps),
+                            std::unique_ptr<Expr>(measurement), false,
+                            std::move(*transitions));
+    delete name;
+    delete params;
+    delete temps;
+    delete transitions;
+    if (param)
+      delete param;
+    return s;
+  }
 };
 
 struct ForLoop {
   std::string variable;
   std::unique_ptr<Expr> iterable;
   std::vector<StateDecl> states;
+
+  ForLoop(std::string var, std::unique_ptr<Expr> iter,
+          std::vector<StateDecl> sts)
+      : variable(std::move(var)), iterable(std::move(iter)),
+        states(std::move(sts)) {}
+
+  static ForLoop *make(std::string *var, Expr *iter,
+                       std::vector<StateDecl> *sts) {
+    auto *f = new ForLoop(std::move(*var), std::unique_ptr<Expr>(iter),
+                          std::move(*sts));
+    delete var;
+    delete sts;
+    return f;
+  }
 };
 
 struct AutotunerDecl {
@@ -218,6 +380,38 @@ struct AutotunerDecl {
         params(std::move(p)), entry_state(std::move(entry)),
         states(std::move(s)), loops(std::move(l)) {}
 
+  static AutotunerDecl *
+  make(std::string *name, std::vector<ParamDecl> *input_params,
+       std::vector<ParamDecl> *output_params,
+       std::vector<std::string> *generic_params,
+       std::vector<SpecDecl> *spec_inputs, std::vector<SpecDecl> *spec_outputs,
+       std::vector<std::string> *requires_clause,
+       std::vector<ParamDecl> *params_block, std::string *entry_clause,
+       std::vector<ForLoop> *loop_list, std::vector<StateDecl> *states) {
+    auto *decl = new AutotunerDecl{std::move(*name),
+                                   std::move(*input_params),
+                                   std::move(*output_params),
+                                   std::move(*generic_params),
+                                   std::move(*spec_inputs),
+                                   std::move(*spec_outputs),
+                                   std::move(*requires_clause),
+                                   std::move(*params_block),
+                                   std::move(*entry_clause),
+                                   std::move(*states),
+                                   std::move(*loop_list)};
+    delete name;
+    delete input_params;
+    delete output_params;
+    delete generic_params;
+    delete spec_inputs;
+    delete spec_outputs;
+    delete requires_clause;
+    delete params_block;
+    delete entry_clause;
+    delete loop_list;
+    delete states;
+    return decl;
+  }
   AutotunerDecl(AutotunerDecl &&) noexcept = default;
   AutotunerDecl &operator=(AutotunerDecl &&) noexcept = default;
   AutotunerDecl(const AutotunerDecl &) = delete;
@@ -226,6 +420,17 @@ struct AutotunerDecl {
 
 struct Program {
   std::vector<AutotunerDecl> autotuners;
+  /**
+   * @brief Makes the entire program from a list of Autotuners.
+   */
+  static Program *make(std::vector<AutotunerDecl> *autotuners) {
+    if (autotuners == nullptr) {
+      return new Program{};
+    }
+    auto *res = new Program{std::move(*autotuners)};
+    delete autotuners;
+    return res;
+  }
 };
 
 } // namespace falcon::atc
--- autotuner/compiler/src/ASTHelpers.cpp
+++ autotuner/compiler/src/ASTHelpers.cpp
@@ -1,22 +1,92 @@
 #include "falcon-atc/ASTHelpers.hpp"
 
 using namespace falcon::atc;
-Program *make_program(std::vector<AutotunerDecl> *autotuners) {
-  if (autotuners == nullptr) {
-    return new Program{};
+
+std::vector<Transition> *
+make_if_transition(Expr *cond, std::vector<Transition> *then_part) {
+  for (auto &t : *then_part) {
+    if (t.condition) {
+      t.condition = std::make_unique<BinaryExpr>("&&", cond->clone(),
+                                                 std::move(t.condition));
+    } else {
+      t.condition = cond->clone();
+    }
   }
-  auto *res = new Program{std::move(*autotuners)};
-  delete autotuners;
-  return res;
+  delete cond;
+  return then_part;
 }
 
-std::vector<AutotunerDecl> *make_autotuner_list() {
-  return new std::vector<AutotunerDecl>();
+std::vector<Transition> *
+make_if_else_transition(Expr *cond, std::vector<Transition> *then_part,
+                        std::vector<Transition> *else_part) {
+  for (auto &t : *then_part) {
+    if (t.condition) {
+      t.condition = std::make_unique<BinaryExpr>("&&", cond->clone(),
+                                                 std::move(t.condition));
+    } else {
+      t.condition = cond->clone();
+    }
+  }
+  auto inv_cond = std::make_unique<UnaryExpr>("!", cond->clone());
+  for (auto &t : *else_part) {
+    if (t.condition) {
+      t.condition = std::make_unique<BinaryExpr>("&&", inv_cond->clone(),
+                                                 std::move(t.condition));
+    } else {
+      t.condition = inv_cond->clone();
+    }
+  }
+  then_part->insert(then_part->end(),
+                    std::make_move_iterator(else_part->begin()),
+                    std::make_move_iterator(else_part->end()));
+  delete else_part;
+  delete cond;
+  return then_part;
 }
 
-std::vector<AutotunerDecl> *append_autotuner(std::vector<AutotunerDecl> *list,
-                                             AutotunerDecl *decl) {
-  list->push_back(std::move(*decl));
-  delete decl;
-  return list;
+std::vector<Transition> *make_simple_transition(SimpleTransitionKind kind,
+                                                std::string *msg) {
+  auto *v = new std::vector<Transition>();
+  Transition t;
+  switch (kind) {
+  case SimpleTransitionKind::Success:
+    t.is_success = true;
+    break;
+  case SimpleTransitionKind::Fail:
+    t.is_fail = true;
+    if (msg) {
+      t.error_message = std::move(*msg);
+      delete msg;
+    }
+    break;
+  case SimpleTransitionKind::Terminal:
+    t.target.state_name = "_TERMINAL_";
+    break;
+  }
+  v->push_back(std::move(t));
+  return v;
+}
+std::vector<Transition> *make_arrow_transition(TransitionTarget *target) {
+  auto *v = new std::vector<Transition>();
+  v->emplace_back(nullptr, std::move(*target), std::vector<Assignment>{});
+  delete target;
+  return v;
+}
+std::vector<Transition> *
+make_assignment_arrow_transition(std::vector<Assignment> *assigns,
+                                 TransitionTarget *target) {
+  auto *v = new std::vector<Transition>();
+  v->emplace_back(nullptr, std::move(*target), std::move(*assigns));
+  delete assigns;
+  delete target;
+  return v;
+}
+std::vector<Transition> *make_assignment_transition(Assignment *asgn) {
+  auto *v = new std::vector<Transition>();
+  std::vector<Assignment> asgns;
+  asgns.push_back(std::move(*asgn));
+  v->emplace_back(nullptr, TransitionTarget("", "", nullptr, {}),
+                  std::move(asgns));
+  delete asgn;
+  return v;
 }
