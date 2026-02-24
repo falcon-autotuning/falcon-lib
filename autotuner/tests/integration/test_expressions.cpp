96c4872e14df88c43d01f113464b340a973fcb4e
tjk144 <tkovach@wisc.edu>
2026-02-24 09:54:27 -0600

    Fixed tests, now expressions are all passing, stuck on control_flow and elif

--- autotuner/tests/integration/test_expressions.cpp
+++ autotuner/tests/integration/test_expressions.cpp
@@ -16,10 +16,10 @@ TEST_F(ExpressionsTest, ArithmeticOperations) {
   ASSERT_NE(params.find("sub"), params.end());
   ASSERT_NE(params.find("mul"), params.end());
   ASSERT_NE(params.find("div"), params.end());
-  EXPECT_DOUBLE_EQ(std::any_cast<double>(params.at("add")), 13.0);
-  EXPECT_DOUBLE_EQ(std::any_cast<double>(params.at("sub")), 7.0);
-  EXPECT_DOUBLE_EQ(std::any_cast<double>(params.at("mul")), 30.0);
-  EXPECT_NEAR(std::any_cast<double>(params.at("div")), 3.333, 0.01);
+  EXPECT_DOUBLE_EQ(std::get<double>(params.at("add")), 13.0);
+  EXPECT_DOUBLE_EQ(std::get<double>(params.at("sub")), 7.0);
+  EXPECT_DOUBLE_EQ(std::get<double>(params.at("mul")), 30.0);
+  EXPECT_NEAR(std::get<double>(params.at("div")), 3.333, 0.01);
 }
 
 TEST_F(ExpressionsTest, ComparisonOperators) {
@@ -35,11 +35,11 @@ TEST_F(ExpressionsTest, ComparisonOperators) {
   ASSERT_NE(params.find("eq"), params.end());
   ASSERT_NE(params.find("gte"), params.end());
   ASSERT_NE(params.find("lte"), params.end());
-  EXPECT_FALSE(std::any_cast<bool>(params.at("gt")));
-  EXPECT_TRUE(std::any_cast<bool>(params.at("lt")));
-  EXPECT_FALSE(std::any_cast<bool>(params.at("eq")));
-  EXPECT_FALSE(std::any_cast<bool>(params.at("gte")));
-  EXPECT_TRUE(std::any_cast<bool>(params.at("lte")));
+  EXPECT_FALSE(std::get<bool>(params.at("gt")));
+  EXPECT_TRUE(std::get<bool>(params.at("lt")));
+  EXPECT_FALSE(std::get<bool>(params.at("eq")));
+  EXPECT_FALSE(std::get<bool>(params.at("gte")));
+  EXPECT_TRUE(std::get<bool>(params.at("lte")));
 }
 
 TEST_F(ExpressionsTest, LogicalOperations) {
@@ -51,7 +51,7 @@ TEST_F(ExpressionsTest, LogicalOperations) {
       "LogicalTest", params, true};
   ASSERT_TRUE(compile_and_run(cenv));
   ASSERT_NE(params.find("result"), params.end());
-  EXPECT_EQ(std::any_cast<std::string>(params.at("result")), "expected");
+  EXPECT_EQ(std::get<std::string>(params.at("result")), "expected");
 }
 
 TEST_F(ExpressionsTest, UnaryOperators) {
@@ -64,8 +64,8 @@ TEST_F(ExpressionsTest, UnaryOperators) {
   ASSERT_TRUE(compile_and_run(cenv));
   ASSERT_NE(params.find("negated"), params.end());
   ASSERT_NE(params.find("inverted"), params.end());
-  EXPECT_EQ(std::any_cast<int64_t>(params.at("negated")), -42);
-  EXPECT_FALSE(std::any_cast<bool>(params.at("inverted")));
+  EXPECT_EQ(std::get<int64_t>(params.at("negated")), -42);
+  EXPECT_FALSE(std::get<bool>(params.at("inverted")));
 }
 
 TEST_F(ExpressionsTest, ComplexExpressions) {
@@ -80,5 +80,5 @@ TEST_F(ExpressionsTest, ComplexExpressions) {
   ASSERT_TRUE(compile_and_run(cenv));
   ASSERT_NE(params.find("result"), params.end());
   // (5 + 3) * 4 - 10 = 8 * 4 - 10 = 32 - 10 = 22
-  EXPECT_EQ(std::any_cast<int64_t>(params.at("result")), 22);
+  EXPECT_EQ(std::get<int64_t>(params.at("result")), 22);
 }
