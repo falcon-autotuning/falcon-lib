45c1b1373c11779400e84c28c5722a0e0d9f1416
tjk144 <tkovach@wisc.edu>
2026-02-25 15:59:17 -0600

    We now have runtime errors in the stmt evaluation exposed for user interpretability

--- autotuner/src/FunctionRegistry.cpp
+++ autotuner/src/FunctionRegistry.cpp
@@ -237,21 +237,21 @@ void register_all_builtins(FunctionRegistry &registry) {
 
   registry.register_builtin(
       "logInfo", [](const ParameterMap &params) -> FunctionResult {
-        std::string format = std::get<std::string>(params.at("format"));
+        std::string format = std::get<std::string>(params.at("arg1"));
         log::info(format);
         return FunctionResult{nullptr};
       });
 
   registry.register_builtin(
       "logWarn", [](const ParameterMap &params) -> FunctionResult {
-        std::string format = std::get<std::string>(params.at("format"));
+        std::string format = std::get<std::string>(params.at("arg1"));
         log::warn(format);
         return FunctionResult{nullptr};
       });
 
   registry.register_builtin(
       "logError", [](const ParameterMap &params) -> FunctionResult {
-        std::string format = std::get<std::string>(params.at("format"));
+        std::string format = std::get<std::string>(params.at("arg1"));
         log::error(format);
         return FunctionResult{nullptr};
       });
