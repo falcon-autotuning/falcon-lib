3f43597e3a8ecb112243307b1c6de83e112bf436
tjk144 <tkovach@wisc.edu>
2026-02-26 21:32:01 -0600

    Thsi does color treesitter

--- autotuner/treesitter/queries/falcon/highlights.scm
+++ autotuner/treesitter/queries/falcon/highlights.scm
@@ -1,50 +1,69 @@
-; ============================================================
-; Keywords
-; ============================================================
-(autotuner_decl "autotuner" @keyword)
-(routine_decl "routine" @keyword)
-(state_decl "state" @keyword)
-(entry_stmt "start" @keyword)
-(terminal_stmt "terminal" @keyword)
-"uses" @keyword
+;; Keywords
+"autotuner"  @keyword
+"routine"    @keyword
+"state"      @keyword
+"start"      @keyword
+"uses"       @keyword
+"terminal"   @keyword
+"if"         @keyword.conditional
+"elif"       @keyword.conditional
+"else"       @keyword.conditional
 
-; Conditionals
-(if_stmt "if" @keyword.conditional)
-(else_clause "else" @keyword.conditional)
-(elif_clause "elif" @keyword.conditional)
-
-; Operators / punctuation
-(transition_stmt "->" @operator)
-(entry_stmt "->" @operator)
-"->" @operator
-
-; ============================================================
-; Types
-; ============================================================
+;; Types
 (type) @type
 
-; ============================================================
-; Function / autotuner / routine names
-; ============================================================
-(autotuner_decl name: (identifier) @function)
-(routine_decl   name: (identifier) @function)
-(state_decl     name: (identifier) @function.method)
-
-; ============================================================
-; Variables / identifiers
-; ============================================================
+;; Identifiers
 (identifier) @variable
 
-; ============================================================
-; Literals
-; ============================================================
-(string_literal) @string
+;; Named fields — highlight declaration names as function/type names
+(autotuner_decl name: (identifier) @type.definition)
+(routine_decl   name: (identifier) @function)
+(state_decl     name: (identifier) @label)
+
+;; Entry / transition targets
+(entry_stmt     target: (identifier) @label)
+(transition_stmt target: (identifier) @label)
+
+;; Parameters
+(param_decl name: (identifier) @variable.parameter)
+
+;; Assignment targets
+(assign_stmt targets: (identifier) @variable)
+
+;; Literals
 (int_literal)    @number
 (float_literal)  @number.float
 (bool_literal)   @boolean
+(string_literal) @string
 (nil_literal)    @constant.builtin
 
-; ============================================================
-; Comments
-; ============================================================
-(comment) @comment @spell
+;; Operators / punctuation
+"->"   @operator
+"="    @operator
+"+"    @operator
+"-"    @operator
+"*"    @operator
+"/"    @operator
+"=="   @operator
+"!="   @operator
+"<"    @operator
+">"    @operator
+"<="   @operator
+">="   @operator
+"&&"   @operator
+"||"   @operator
+"!"    @operator
+
+;; Comments
+(comment) @comment
+
+;; Brackets
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+";" @punctuation.delimiter
+"," @punctuation.delimiter
+"." @punctuation.delimiter
