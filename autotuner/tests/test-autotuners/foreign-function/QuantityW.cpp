717370dedb0ca4af078b24753c777665d37a8b6d
tjk144 <tkovach@wisc.edu>
2026-02-27 13:30:18 -0600

    updated the fal tests to include import and struct and ffimport tests

---
+++ autotuner/tests/test-autotuners/foreign-function/QuantityW.cpp
@@ -0,0 +1,20 @@
+#include "falcon-autotuner/RuntimeValue.hpp"
+struct Quantity {
+  int64_t a;
+  int64_t b;
+};
+
+falcon::autotuner::FunctionResult
+STRUCTQuantityNew(falcon::autotuner::ParameterMap params) {
+  int64_t a = std::get<int64_t>(params.at("a"));
+  int64_t b = std::get<int64_t>(params.at("b"));
+  std::shared_ptr<Quantity> q = std::make_shared<Quantity>(a, b);
+  return falcon::autotuner::FunctionResult{q};
+}
+
+falcon::autotuner::FunctionResult
+STRUCTQuantityValue(falcon::autotuner::ParameterMap params) {
+  using QuantitySP = std::shared_ptr<Quantity>;
+  QuantitySP q = std::get<QuantitySP>(params.at("quantity"));
+  return falcon::autotuner::FunctionResult{q->a};
+}
