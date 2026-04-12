1e2fb6dd6d11a457500cf1ee7e9733816045a105
tjk144 <tkovach@wisc.edu>
2026-04-12 13:35:14 -0500

    Added removed dependancies to vcpkg for install so they are still placed into the package

--- ports/falcon-routine/portfile.cmake
+++ ports/falcon-routine/portfile.cmake
@@ -1,14 +1,12 @@
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO falcon-autotuning/cereal-xtensor
+    REPO falcon-autotuning/falcon-routine
     REF v${VERSION}
-    SHA512 28856ce19074b9dd5156c51bd34d56574997f6b780fd64aa7a53b073bdb90b4077d571bc8c8436d13f77af4898d249a8c735cbad31080aca35a18b6da2ef888a)
-
+    SHA512 8011515560cd839399845f3336573cc06b49b6f5963e57b2e6603273358a313ff46f0be36316c62c49ebfb7995589874f629d7880f09cb3455f65aa3a39c3caa
+)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
-
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 file(INSTALL "${SOURCE_PATH}/LICENSE"
