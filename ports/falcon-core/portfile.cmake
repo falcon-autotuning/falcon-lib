e15437b9e8a98045a82fffc6e67f566abf3369b3
Daniel Schug <daschug1@gmail.com>
2026-04-15 12:36:11 -0400

    fix: updated falcon-core hash stuff

--- ports/falcon-core/portfile.cmake
+++ ports/falcon-core/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/falcon-core
     REF v${VERSION}
-    SHA512 5bef087c0ec3bf7b171977931dfd851a6a9d025043284532698abc9c21222fd4778a0ba61c301025e5d42c4f98b2f8d55b54d7975f44c957088712fe41a16c62)
+    SHA512 82c65dfbc93afffa932a8cfbbce18b67f9991443792ceb9d0744fc6ee9cadcce0021330554693e689625e8f473fce729b82775b0de1e555df0e79181ba7049f6)
 
 set(BUILD_C_API OFF)
 if("c-api" IN_LIST FEATURES)
@@ -13,6 +13,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DFALCON_CORE_BUILD_C_API=${BUILD_C_API}
+        -DBUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
