99022743140ec9ef4271ef001e8d32b8fe1661b3
Daniel Schug <daschug1@gmail.com>
2026-04-15 11:58:08 -0400

    fix: updated stale hashes

--- ports/falcon-database/portfile.cmake
+++ ports/falcon-database/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/falcon-database
     REF v${VERSION}
-    SHA512 0
+    SHA512 e1f4e86e219dcb359564775e27db9d66f59fe301db59b59ea17bc28a74986b8ac629c65da0336398cfcfb31e4ea089a1936642fd684c042792bda1ebd0ae7fdd
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
