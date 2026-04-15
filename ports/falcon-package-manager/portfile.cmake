e0a08e86aa62cabfd0f32b8ac4432d353bb635d2
Daniel Schug <daschug1@gmail.com>
2026-04-15 16:01:42 -0400

    fix: pm hash

--- ports/falcon-package-manager/portfile.cmake
+++ ports/falcon-package-manager/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/falcon-package-manager
     REF v${VERSION}
-    SHA512 059f7c7e4ba38ba99d4a055e947ab85123a8d2cbf0e46e2e976892d8ea02544c172550ef632842d7fed87f6258af2eab6b3aaebf83bc67ca3c2d76cbc7db234c
+    SHA512 12a532016930fbaec680b5692e24f24c0994e05bdc33ddabaee8b472164ea11f4c48274be3fd5c28415143657770d7495384fdef0437bfd53e5e2aa97656bdd4
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
